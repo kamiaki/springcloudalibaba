51368162d1f3ea7eae9e8880949fc8c86d12f847
kamiaki <247557768@qq.com>
2021-08-10 15:17:39 +0800

    123

--- app1/src/main/java/com/aki/App1Application.java
+++ app1/src/main/java/com/aki/App1Application.java
@@ -2,9 +2,12 @@ package com.aki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 // 测试gitee提交1
 @SpringBootApplication
+// nacos 客户端
+@EnableDiscoveryClient
 public class App1Application {
     public static void main(String[] args) {
         SpringApplication.run(App1Application.class, args);
--- app2/src/main/java/com/aki/App2Application.java
+++ app2/src/main/java/com/aki/App2Application.java
@@ -3,11 +3,14 @@ package com.aki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 // 测试gitee提交1
 @SpringBootApplication
-public class App1Application {
+// nacos 客户端
+@EnableDiscoveryClient
+public class App2Application {
     public static void main(String[] args) {
-        SpringApplication.run(App1Application.class, args);
+        SpringApplication.run(App2Application.class, args);
     }
 }
