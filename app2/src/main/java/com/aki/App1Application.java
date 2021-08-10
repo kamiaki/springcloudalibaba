75e091f09b64981c7f78a0ee0ea8b5b36b390b35
kamiaki <247557768@qq.com>
2021-08-10 14:20:55 +0800

    123

---
+++ app2/src/main/java/com/aki/App1Application.java
@@ -0,0 +1,13 @@
+package com.aki;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 测试gitee提交1
+@SpringBootApplication
+public class App1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(App1Application.class, args);
+    }
+}
